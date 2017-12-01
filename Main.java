cee1e36ea35c6053f4089951565df210981ab153
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-12-01 22:34:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        CoffeeMachine CM = new CoffeeMachine();
+        CM.exec("5");
+        CM.exec("5");
+        CM.exec("5");
+        CM.exec("5");
+        CM.exec("10");
     }
 }
