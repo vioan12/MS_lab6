964cfad74baaf7c7ba31de240ada78199fc4aa7c
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-12-01 22:35:16 +0200

    Update PetriNet.java

--- PetriNet.java
+++ PetriNet.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,10 +10,14 @@ public abstract class PetriNet
 
     protected void exec(String actiune)
     {
+        List<Tranzitie> tempTranzitii= new ArrayList<Tranzitie>();
         for(int i=0;i<Tranzitii.size();i++){
             if(Tranzitii.get(i).Get_tag()==actiune)
                 if (Tranzitii.get(i).isValid()==true)
-                    Tranzitii.get(i).update();
+                    tempTranzitii.add(Tranzitii.get(i));
+        }
+        for(int i=0;i<tempTranzitii.size();i++){
+            tempTranzitii.get(i).update();
         }
     }
 }
