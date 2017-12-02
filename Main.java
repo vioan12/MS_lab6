7e225ab804a6d84060c6737c41c515af2590b9e1
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-12-02 03:43:18 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,39 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        CoffeeMachine CM = new CoffeeMachine();
-        CM.exec("5");
-        CM.exec("5");
-        CM.exec("5");
-        CM.exec("5");
-        CM.exec("10");
+        try {
+
+            CoffeeMachine CM = new CoffeeMachine();
+            Scanner scanner = new Scanner(System.in);
+            String input;
+            String suma1,suma2;
+            System.out.println("Puteti cumpara urmatoarele produse:");
+            System.out.println("Ciocolata cu lapte: 10 bani");
+            System.out.println("Espresso scurt: 15 bani");
+            System.out.println("Puteti folosti numai monede de 10 si 5 bani");
+            System.out.println("");
+            System.out.println("Introduceti optiunea dorita:");
+            input = scanner.next();
+            while (!input.equals("$")) {
+                suma1=CM.taglocatiejeton();
+                CM.exec(input);
+                suma2=CM.taglocatiejeton();
+                if(Integer.parseInt(suma1)-Integer.parseInt(suma2)==10){
+                    System.out.println("Ciocolata cu lapte va este servita");
+                }
+                if(Integer.parseInt(suma1)-Integer.parseInt(suma2)==15){
+                    System.out.println("Espresso scurt va este servit");
+                }
+                System.out.println("Suma actuala:"+CM.taglocatiejeton());
+                System.out.println("");
+                System.out.println("Introduceti optiunea dorita:");
+                input = scanner.next();
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
