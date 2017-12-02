c83553d7a0cc83b2beadfde3e586f0db13130cd5
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-12-02 03:44:23 +0200

    Update PetriNet.java

--- PetriNet.java
+++ PetriNet.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,12 +14,27 @@ public abstract class PetriNet
     {
         List<Tranzitie> tempTranzitii= new ArrayList<Tranzitie>();
         for(int i=0;i<Tranzitii.size();i++){
-            if(Tranzitii.get(i).Get_tag()==actiune)
+            if(Tranzitii.get(i).Get_tag().equals(actiune))
                 if (Tranzitii.get(i).isValid()==true)
                     tempTranzitii.add(Tranzitii.get(i));
         }
         for(int i=0;i<tempTranzitii.size();i++){
             tempTranzitii.get(i).update();
         }
+    }
+    protected List<Locatie> starejetoatelocatii()
+    {
+        List<Locatie> listalocatii = new ArrayList<Locatie>();
+        for(int i=0;i<Tranzitii.size();i++) {
+            for (int j = 0; j < Tranzitii.get(i).Get_Arce().size(); j++) {
+                listalocatii.add(Tranzitii.get(i).Get_Arce().get(j).Get_locatie());
+
+            }
+        }
+        for(int i=0;i<listalocatii.size()-1;i++)
+            for(int j=0;j<listalocatii.size();j++)
+                if(listalocatii.get(i).Get_tag().equals(listalocatii.get(j).Get_tag()))
+                    listalocatii.remove(j);
+        return listalocatii;
     }
 }
